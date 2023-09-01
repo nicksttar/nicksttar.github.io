8195f6a1999fffe8099b5f3264498e713913d797
Nick <113777798+nicksttar@users.noreply.github.com>
2023-09-01 16:37:49 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -89,8 +89,6 @@ function custom() {
 let order_finish = document.getElementById('order_finish');
 
 function endfunction() {
-    tg.sendData(JSON.stringify({'1': '1'}));
-    tg.close();
     let name_user = document.getElementById('user_name').value;
     let mail = document.getElementById('user_email').value;
     let phone = document.getElementById('user_phone').value;
